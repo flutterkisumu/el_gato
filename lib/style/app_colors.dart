90eae2723a781524bdbe948e75a1ece449786a9c
Samuel Baraka <sababuvercetti@gmail.com>
2022-11-24 11:23:21 +0300

    feat: Added color palettes

---
+++ lib/style/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static Color cyanColor = Color(0xff00ddb3);
+  static Color darkCyanColor = Color(0xff009d91);
+  static Color blueColor = Color(0xff0092e4);
+  static Color darkBlueColor = Color(0xff0657A0);
+}
