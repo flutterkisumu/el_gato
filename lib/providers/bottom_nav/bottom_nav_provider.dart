86a9701339e4e9a2b8099c3e84c7071096960fb8
Samuel Baraka <sababuvercetti@gmail.com>
2022-12-16 09:55:31 +0300

    fix: Simplified the main bottom nav provider

--- lib/providers/bottom_nav/bottom_nav_provider.dart
+++ lib/providers/bottom_nav/bottom_nav_provider.dart
@@ -1,18 +1,4 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 /// The main bottom nav bar provider
-final mainBottomNavProvider =
-    StateNotifierProvider<BottomNavProvider, int>((_) {
-  return BottomNavProvider();
-});
-
-/// This provider holds the bottom nav state
-class BottomNavProvider extends StateNotifier<int> {
-  /// The constructor
-  BottomNavProvider() : super(0);
-
-  /// Change the bottom nav state
-   void setvalue(int val) {
-    state = val;
-  }
-}
+final mainBottomNavProvider = StateProvider((ref) => 0);
