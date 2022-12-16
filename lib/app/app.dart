34afcaf89f2d00db025762060eaaa218f996b660
Samuel Baraka <sababuvercetti@gmail.com>
2022-12-16 12:07:43 +0300

    feat: Registered the dio provider inside getit

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,15 +1,34 @@
+import 'package:dio/dio.dart';
+import 'package:el_gato/providers/dio_provider/dio_provider.dart';
 import 'package:el_gato/router/router.gr.dart';
 import 'package:el_gato/style/app_colors.dart';
 import 'package:flutter/material.dart';
 // ignore: depend_on_referenced_packages
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:form_builder_validators/form_builder_validators.dart';
+import 'package:get_it/get_it.dart';
 
 /// The main app
-class MyCatApp extends StatelessWidget {
+class MyCatApp extends ConsumerStatefulWidget {
   /// the constructor
-  MyCatApp({Key? key}) : super(key: key);
+  const MyCatApp({Key? key}) : super(key: key);
+
+  @override
+  ConsumerState<MyCatApp> createState() => _MyCatAppState();
+}
+
+class _MyCatAppState extends ConsumerState<MyCatApp> {
   final _appRouter = AppRouter();
+
+  @override
+  void initState() {
+    super.initState();
+
+    /// Register the dio provider inside getit
+    GetIt.I.registerSingleton<Dio>(ref.read(dioProvider));
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
