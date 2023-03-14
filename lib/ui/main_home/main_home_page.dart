35c956d71fdc5a780dd286b49bf5076d1faab355
Samuel Baraka <sababuvercetti@gmail.com>
2023-03-14 11:56:13 +0300

    fix: Removed unneccessary code

--- lib/ui/main_home/main_home_page.dart
+++ lib/ui/main_home/main_home_page.dart
@@ -1,64 +1,102 @@
 // ignore_for_file: use_colored_box
 
 import 'package:el_gato/providers/bottom_nav/bottom_nav_provider.dart';
-import 'package:el_gato/ui/main_home/feed/feed_page.dart';
+import 'package:el_gato/ui/main_home/feed/images/images_page.dart';
+import 'package:el_gato/ui/main_home/feed/read_page.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'feed/breeds/breeds_page.dart';
-
 /// This is the landing page
 class MainHomePage extends ConsumerWidget {
   /// The constructor
-  MainHomePage({Key? key}) : super(key: key);
+  MainHomePage({super.key});
+
+  /// The bucket to store the page states
+  final PageStorageBucket bucket = PageStorageBucket();
 
   /// The items
   final items = [
-    BottomNavItem(icon: const Icon(CupertinoIcons.home), label: 'Home'),
     BottomNavItem(
-      icon: const Icon(CupertinoIcons.list_bullet_below_rectangle),
-      label: 'Feed',
+      icon: const Icon(
+        CupertinoIcons.home,
+        color: Colors.white,
+      ),
+      tooltip: 'Home',
+      label: 'Home',
     ),
-    BottomNavItem(icon: const Icon(CupertinoIcons.search), label: 'Search'),
     BottomNavItem(
-      icon: const Icon(CupertinoIcons.settings),
-      label: 'Settings',
+      icon: const Icon(
+        CupertinoIcons.book,
+        color: Colors.white,
+      ),
+      label: 'Read',
+      tooltip: 'Read more about cats',
     ),
+    // BottomNavItem(
+    //   icon: const Icon(
+    //     CupertinoIcons.search,
+    //     color: Colors.white,
+    //   ),
+
+    //   label: 'Search',
+    // ),
+    // BottomNavItem(
+    //   icon: const Icon(
+    //     CupertinoIcons.settings,
+    //     color: Colors.white,
+    //   ),
+    //   label: 'Settings',
+    // ),
   ];
 
   /// The pages
   final pages = <Widget>[
-    Container(
-      color: Colors.red,
+    const ImagesPage(
+      key: PageStorageKey<String>('imagesPage'),
     ),
-    FeedPage(),
-    Container(
-      color: Colors.green,
-    ),
-    Container(
-      color: Colors.orange,
-    ),
+    ReadPage(),
   ];
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Scaffold(
-      body: pages[ref.watch(mainBottomNavProvider)],
-      bottomNavigationBar: CupertinoTabBar(
-        activeColor: Theme.of(context).colorScheme.secondary,
-        currentIndex: ref.watch(mainBottomNavProvider),
-        onTap: (val) {
-          ref.read(mainBottomNavProvider.notifier).state = val;
-        },
-        items: items
-            .map(
-              (e) => BottomNavigationBarItem(
-                icon: e.icon,
-                label: e.label,
+    return PageStorage(
+      bucket: bucket,
+      child: Scaffold(
+        body: CustomScrollView(
+          slivers: [
+            const SliverAppBar(
+              pinned: true,
+              floating: true,
+              expandedHeight: 200,
+              flexibleSpace: FlexibleSpaceBar(
+                title: Text('El Gato'),
               ),
-            )
-            .toList(),
+            ),
+            SliverFillRemaining(
+              child: PageStorage(
+                bucket: bucket,
+                child: pages[ref.watch(mainBottomNavProvider)],
+              ),
+            ),
+          ],
+        ),
+        bottomNavigationBar: NavigationBar(
+          selectedIndex: ref.watch(mainBottomNavProvider),
+          onDestinationSelected: (value) {
+            ref.read(mainBottomNavProvider.notifier).state = value;
+          },
+          destinations: items
+              .map(
+                (e) => NavigationDestination(
+                  tooltip: items[items.indexOf(e)].tooltip,
+                  icon: items[items.indexOf(e)].icon,
+                  selectedIcon: items[items.indexOf(e)].icon,
+                  label: items[items.indexOf(e)].label,
+                ),
+              )
+              .toList(),
+        ),
       ),
     );
   }
@@ -68,11 +106,15 @@ class MainHomePage extends ConsumerWidget {
 
 class BottomNavItem {
   /// Constructor
-  BottomNavItem({required this.icon, required this.label});
+  BottomNavItem(
+      {required this.icon, required this.label, required this.tooltip});
 
   /// The icon
   final Icon icon;
 
   /// The label
   final String label;
+
+  /// The tooltip
+  final String tooltip;
 }
